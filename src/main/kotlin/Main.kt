22ea04a189a16767a5d19d0555858102fa50a4fb
bellaja <bellaja185@gmail.com>
2024-03-12 00:07:02 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+package ru.netology
+fun main(args: Array<String>) {
+    println(calculatetotalPrice(50000, 1, true))
+
+}
+
+val discount = 100
+val premiumDiscount = 1 - 0.05
+val vipDiscount = 1 - 0.01
+var totalPrice = 0
+fun calculatetotalPrice(amount: Int, itemCount: Int, buyer: Boolean): Int {
+    val result = amount * itemCount
+    when {
+        amount > 0 && amount < 1001 -> if (buyer === false) totalPrice = result
+        amount > 1000 && amount < 10001 -> if (buyer === false) totalPrice = result - discount
+        amount > 10_000 -> if (buyer === false) {
+            totalPrice = (result * premiumDiscount).toInt()
+        } else totalPrice = ((result * premiumDiscount) * vipDiscount).toInt()
+    }
+    return totalPrice
+}
+
+
+
+
+
